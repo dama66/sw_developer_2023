89fd7343a1cd42bf0e4ac09a71d4765944c7cf3c
Atilla  (Home) <atilla@kati.bz>
2023-09-28 20:56:50 +0200

    Beispiele vom 26.09.2023 - erster Abend Einstieg

---
+++ src/20230926/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Wifi.Application
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Hallo Leute..");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ src/20230926/HelloWorldApplication/HelloWorldApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo liebe Leute....");
+            Console.WriteLine("C# ist toll...");            
+
+            Console.ReadLine();
+        }
+    }
+}
