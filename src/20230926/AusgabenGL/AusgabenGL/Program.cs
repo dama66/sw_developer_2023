b83f7ee34ec5f57d650d8c80af3d984bbf535700
David Maier <davidmaier66@gmail.com>
2023-10-03 19:56:57 +0200

    Grundlagen WriteLine

---
+++ src/20230926/AusgabenGL/AusgabenGL/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AusgabenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+            Console.WriteLine("\tName:\t\t[Maier David]");
+            Console.WriteLine("\tAlter:\t\t[38] Jahre");
+            Console.WriteLine();
+            Console.WriteLine("\tWohnort:\t[Sulz]");
+            Console.WriteLine();
+            Console.WriteLine("C# ist super!");
+            Console.ReadLine();
+
+            /*
+             *    Name:    [Teilnehmer Name]
+             *    Alter:   [Teilnehmer Alter] Jahr 
+             * 
+             *    Wohnort: [Teilnehmer Wohnort]
+             * 
+             * Hier soll ein Kommentar zu C# stehen!
+             * 
+             */
+        }
+    }
+}
