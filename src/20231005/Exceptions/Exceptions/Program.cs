52984c47b4997419479192ad32c38090afe98251
David Maier <davidmaier66@gmail.com>
2023-10-05 21:33:11 +0200

    grundlagen try catch

---
+++ src/20231005/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 0;
+
+Eingabe:
+            Console.Write("Alter eingeben:");
+
+            try 
+            {
+                alter = int.Parse(Console.ReadLine());
+            }
+
+
+            catch (OverflowException oex)
+            { 
+                Console.WriteLine(oex.Message);
+                goto Eingabe;
+            }
+            catch (FormatException fex)
+            {
+                Console.WriteLine(fex.Message);
+                goto Eingabe;
+            }
+            catch (ArgumentException aex)
+            {
+                Console.WriteLine(aex.Message);
+                goto Eingabe;
+            }
+
+
+        }
+    }
+}
