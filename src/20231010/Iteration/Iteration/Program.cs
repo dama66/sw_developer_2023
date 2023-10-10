cf4b5eabe653a89bada2ebb364f3cd949ea9abf9
maierda <davidmaier66@gmail.com>
2023-10-10 19:29:33 +0200

    InterationGL

---
+++ src/20231010/Iteration/Iteration/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+         for (int i = 0; i < 5; i++) 
+            {
+                Console.WriteLine(i);
+            }
+
+         string input = string.Empty;
+
+         while (input.Length <5)
+            {
+                Console.Write("Ihre Eingabe:");
+                input = Console.ReadLine();
+            }
+
+            do
+            {
+                Console.Write("Ihre Eingabe:");
+                input = Console.ReadLine();
+            }
+            while (input.Length < 5);
+        }
+    }
+}
