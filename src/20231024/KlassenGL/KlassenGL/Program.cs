4b3ad7511b59466c7cc0903fb69ed018874cde95
maierda <davidmaier66@gmail.com>
2023-10-24 21:46:11 +0200

    Grundlagen1 Klassen

---
+++ src/20231024/KlassenGL/KlassenGL/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            book.StartBorrowing(TimeSpan.FromDays(14));
+            book.DisplayInfos();
+
+
+        }
+    }
+}
