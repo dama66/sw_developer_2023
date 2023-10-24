6c8d68d8574829ebcfce435d2a50a01237551a6b
maierda <davidmaier66@gmail.com>
2023-10-24 18:26:02 +0200

    foreach grundlagen

---
+++ src/20231024/foreachGL/foreachGL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreachGL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklaration, Dimensionierung, Initialisierung
+            string[] names = new string[]
+            {
+                "David", "Alex", "Adrina", "Valentina"
+            };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
