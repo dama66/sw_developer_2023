58b4a44c766735aeeb7dc93ba1cf0637d8381589
David Maier <davidmaier66@gmail.com>
2023-10-24 10:50:32 +0200

    processed all TODO´s

--- src/20231019/Teilnehmerverwaltung V2.0/Teilnehmerverwaltung V2.0/Program.cs
+++ src/20231019/Teilnehmerverwaltung V2.0/Teilnehmerverwaltung V2.0/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,36 +30,16 @@ namespace Teilnehmerverwaltung_V2._0
 
         static void Main(string[] args)
         {
-            int teilnehmerCount = 0;
-            Teilnehmer teilnehmer = new Teilnehmer();
-            Teilnehmer[] teilnehmerListe;
+
             string headerText = "Teilnehmerverwaltung v2.0 @2023 WIFI-Soft";
 
             //Header ausgeben
             CreateHeader(headerText, ConsoleColor.Yellow, true);
 
-            //Abfrage Anzahl Teilnehmer
-            teilnehmerCount = ReadInt("Bitte Anzahl der Teilnehmer eingeben: ");
+            //main Menu
+            StartMenu();
 
-            //Teilnehmerliste vorbereiten
-            teilnehmerListe = new Teilnehmer[teilnehmerCount];
-
-            //Daten einlesen
-            Console.WriteLine("\nBitte geben Sie die Teilnehmer Daten ein:");
-
-            teilnehmerListe = GetStudentInfos(teilnehmerCount);
-            
-            //Ausgabe der Daten
-            Console.WriteLine("\n\nDie Teilnehmerdaten:");
-            DisplayStudentInfo(teilnehmerListe);
-
-            //TODO: implement .json and .xml too!!
-            SaveStudentInfoToFile(teilnehmerListe, "Teilnehmerliste", Format.csv);
-
-            //TODO: Datei mit StreamReader einlesen (LoadStudentInfoFromFile)
         }
-
-       
 
         private static void CreateHeader(string headerText, ConsoleColor headerTextColor, bool clearScreen)
         {
@@ -89,6 +70,49 @@ namespace Teilnehmerverwaltung_V2._0
             Console.SetCursorPosition(0, 4);
             //set console title
             Console.Title = headerText;
+        }
+
+        private static void StartMenu()
+        {
+            int teilnehmerCount = 0;
+            Teilnehmer teilnehmer = new Teilnehmer();
+            Teilnehmer[] teilnehmerListe;
+
+            string prompt = "What do you want to do?";
+            string[] options = { "Add new student", "Read file" };
+
+            Menu MainMenu = new Menu(prompt, options);
+            int selectedIndex = MainMenu.Run();
+
+            if (selectedIndex == 0)
+            {
+                Console.Clear();
+                //Abfrage Anzahl Teilnehmer
+                teilnehmerCount = ReadInt("Bitte Anzahl der Teilnehmer eingeben: ");
+
+                //Teilnehmerliste vorbereiten
+                teilnehmerListe = new Teilnehmer[teilnehmerCount];
+
+                //Daten einlesen
+                Console.WriteLine("\nBitte geben Sie die Teilnehmer Daten ein:");
+
+                teilnehmerListe = GetStudentInfos(teilnehmerCount);
+
+                //Ausgabe der Daten
+                Console.WriteLine("\n\nDie Teilnehmerdaten:");
+                DisplayStudentInfo(teilnehmerListe);
+
+                //save to file
+                SaveToFile(teilnehmerListe);
+            }
+            else
+            {
+                //get files
+                List<string> fileList = GetFiles(Environment.CurrentDirectory, false);
+
+                //read file
+                ReadFile(fileList);
+            }
         }
 
         private static DateTime ReadDateTime(string inputPrompt, string inputFormat)
@@ -225,19 +249,24 @@ namespace Teilnehmerverwaltung_V2._0
             }
         }
 
-        private static void SaveStudentInfoToFile(Teilnehmer[] students, string filename)
+        private static void SaveToFile(Teilnehmer[] teilnehmerListe)
         {
-            // Dateizugriff immer mit "using". Damit ist sichergestellt dass die Datei nach ende oder bei Absturz die Datei wieder freigegeben wird.
-            using (StreamWriter sw = new StreamWriter(filename, true))
-            {
-                for (int i = 0; i < students.Length; i++)
-                {
-                    string dataline = $"{students[i].Vorname}, {students[i].Name}, {students[i].Gebutsdatum.ToShortDateString()}, {students[i].Plz}, {students[i].Ort}";
-                    sw.WriteLine(dataline);
-                }
-            }
+            string prompt = "Do you want to save students to file?";
+            string[] options = { "Yes", "No" };
 
-            
+            Menu SelectSave = new Menu(prompt, options);
+            int selectedIndex = SelectSave.Run();
+
+            if (selectedIndex == 0)
+            {
+                string prompt1 = "Please choose the Format";
+                string[] options1 = { "csv", "json", "xml" };
+
+                Menu SelectFormat = new Menu(prompt1, options1);
+                int selectedFormat = SelectFormat.Run();
+
+                SaveStudentInfoToFile(teilnehmerListe, "Teilnehmerliste", (Format)selectedFormat);
+            }
         }
 
         private static void SaveStudentInfoToFile(Teilnehmer[] students, string filename, Format format)
@@ -258,17 +287,77 @@ namespace Teilnehmerverwaltung_V2._0
                     for (int i = 0; i < students.Length; i++)
                     {
                         string dataline =
-                            "{\n\"first name\": \"" + students[i].Vorname + "\"\n\"last name\": \"" + students[i].Name +
-                            "\"\n\"birth date\": \"" + students[i].Gebutsdatum.ToShortDateString() +
-                            "\"\n\"postal code\": \"" + students[i].Plz + "\"\n\"residence\": \"" + students[i].Ort +
-                            "}\n";
+                            "{\n" +
+                            "\"first name\": \"" + students[i].Vorname + "\"\n\"last name\": \"" + students[i].Name + "\"\n" +
+                            "\"birth date\": \"" + students[i].Gebutsdatum.ToShortDateString() + "\"\n" +
+                            "\"postal code\": \"" + students[i].Plz + "\"\n" +
+                            "\"residence\": \"" + students[i].Ort + "\"\n},";
                         sw.WriteLine(dataline);
                     }
                 }
                 else if (format == Format.xml)
                 {
-
+                    for (int i = 0; i < students.Length; i++)
+                    {
+                        string dataline =
+                            "\n<firstName>" + students[i].Vorname + @"\n<\firstName>" +
+                            "\n<lastName>" + students[i].Name + @"\n<\lastName>" +
+                            "\n<birthDate>" + students[i].Gebutsdatum.ToShortDateString() + @"\n<\birthDate>" +
+                            "\n<postalCode>" + students[i].Plz + @"\n<\postalCode>" +
+                            "\n<residence>" + students[i].Ort + @"\n<\residence>";
+                        sw.WriteLine(dataline);
+                    }
                 }
+            }
+        }
+
+        private static List<string> GetFiles(string root, bool subFolders)
+        {
+            List<string> FileArray = new List<string>();
+            try
+            {
+                string[] Files = System.IO.Directory.GetFiles(root);
+                string[] Folders = System.IO.Directory.GetDirectories(root);
+
+                for (int i = 0; i < Files.Length; i++)
+                {
+                    if (Files[i].Contains(".csv") || Files[i].Contains(".json") || Files[i].Contains(".xml"))
+                    FileArray.Add(Files[i].ToString());
+                }
+
+                if (subFolders == true)
+                {
+                    for (int i = 0; i < Folders.Length; i++)
+                    {
+                        FileArray.AddRange(GetFiles(Folders[i], subFolders));
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                throw (Ex);
+            }
+            return FileArray;
+        }
+
+        private static void ReadFile(List<string> fileList)
+        {
+            string prompt = "select the file you want to read:";
+            string[] optionsRaw = fileList.ToArray();
+            string[] options = new string[optionsRaw.Length];
+
+            for (int i = 0; i < optionsRaw.Length; i++)
+            {
+                options[i] = optionsRaw[i].Replace(Environment.CurrentDirectory, "");
+            }
+
+            Menu SelectFile = new Menu(prompt, options);
+            int selectedIndex = SelectFile.Run();
+
+            using (StreamReader sr = new StreamReader(new FileStream(optionsRaw[selectedIndex], FileMode.Open)))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+                Console.ReadLine();
             }
         }
     }
