5375733e8cb936749edb864a914cffd4a434f6f9
maierda <davidmaier66@gmail.com>
2023-11-07 21:24:54 +0100

    Vererbung BSP

---
+++ src/20231107/Shapes/Shapes/Shapes/Dreieck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shapes.Shapes
+{
+    internal class Dreieck : VielEck
+    {
+        public Dreieck(ConsoleColor dreieckColour)
+                : base("Dreieck", 3, dreieckColour)
+        {
+
+        }
+    }
+}
---
+++ src/20231107/Shapes/Shapes/Shapes/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Shapes.Shapes
+{
+    internal class Shape
+    {
+        private string _description;
+        private int _cornerCount;
+        private ConsoleColor _color;
+
+        public Shape(string description, int cornerCount) 
+            : this(description, cornerCount, ConsoleColor.Green) { }
+        public Shape (string description, int cornerCount, ConsoleColor color)
+        {
+            _description = description;
+            _cornerCount = cornerCount;
+            _color = color;
+        }
+
+        public string Description { get { return _description; } }
+        public int CornerCount { get { return _cornerCount; } }
+        public ConsoleColor Colour { get { return _color; } set { _color = value; ; }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine($"[{_description}] Corners: {_cornerCount}");
+        }
+
+    }
+}
