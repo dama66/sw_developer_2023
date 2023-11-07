4eff68379374d08ed8a01edd0eea6db7d4005009
maierda <davidmaier66@gmail.com>
2023-11-07 20:29:42 +0100

    klassische Vererbung

---
+++ src/20231107/Fahrzeugverwaltung/Fahrzeugverwaltung/Bike.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeugverwaltung
+{
+    internal class Bike : Vehicle
+    {
+        private int _suspensionTravel;
+
+        public Bike() 
+            : base("Vespa", ConsoleColor.DarkYellow, 5, 45) 
+        {
+            _suspensionTravel = 80;
+        }
+
+        public Bike (int suspensionTravel, string bikeDescription, ConsoleColor bikeColour, int ps, int maxSpeed) 
+            : base(bikeDescription, bikeColour, ps, maxSpeed)
+        {
+            _suspensionTravel = suspensionTravel;
+        }
+
+        public int SuspensionTravel { get {  return _suspensionTravel; } }
+
+    }
+}
