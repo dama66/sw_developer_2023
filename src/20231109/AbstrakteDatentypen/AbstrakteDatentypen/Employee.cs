63b6d39f2d232f3b027eac3aeb6498bc810846ad
maierda <davidmaier66@gmail.com>
2023-11-09 21:34:35 +0100

    Poymorphie

---
+++ src/20231109/AbstrakteDatentypen/AbstrakteDatentypen/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AbstrakteDatentypen
+{
+    internal abstract class Employee
+    {
+        public abstract string Name { get; }
+
+        public abstract Guid Id { get; }
+
+        public abstract int BirthYear { get; }
+
+        public abstract decimal CalculateSallery();
+
+
+        public abstract void ShowInfo();
+
+    }
+}
