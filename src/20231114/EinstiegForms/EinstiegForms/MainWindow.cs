291c80a2745443755c4522f75ec538e1d033c9ca
maierda <davidmaier66@gmail.com>
2023-11-14 19:45:45 +0100

    Grundlagen Forms Projekte

---
+++ src/20231114/EinstiegForms/EinstiegForms/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EinstiegForms
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btt_ok_Click(object sender, EventArgs e)
+        {
+            lbl_result.Text = "Ergebnis: " + txt_name.Text;
+            txt_name.Text = string.Empty;
+            txt_name.Focus();
+        }
+    }
+}
