066e5b2af41572c5898cb1dff9d385d4c867a424
maierda <davidmaier66@gmail.com>
2024-01-16 19:49:40 +0100

    MEthods completed

--- src/Swd.TimeManager/Swd.TimeManager.GuiConsole/Swd.TimeManager.Repository/GenericRepository.cs
+++ src/Swd.TimeManager/Swd.TimeManager.GuiConsole/Swd.TimeManager.Repository/GenericRepository.cs
@@ -2,41 +2,81 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Swd.TimeManager.Repository
 {
     public class GenericRepository<TEntity, TModel> : IGenericRepository<TEntity>
-        where TEntity : class, new() 
+        where TEntity : class, new()
         where TModel : DbContext, new()
 
     {
-        public DbSet<TEntity> DbSet => throw new NotImplementedException();
+        //Member
+        private DbContext _dbContext;
+        private DbSet<TEntity> _dbSet;
+
+        //Properties
+        public DbSet<TEntity> DbSet
+        {
+            get { return _dbSet; }
+        }
+
+        //Konstruktoren
+        public GenericRepository()
+        {
+            Init(new TModel());
+        }
+        //Konstruktoren
+        public GenericRepository(DbContext dbContext)
+        {
+            Init(dbContext);
+        }
+
+        public void Init(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = dbContext.Set<TEntity>();
+        }
 
         public void Add(TEntity t)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(object key)
-        {
-            throw new NotImplementedException();
+            _dbSet.Add(t);
+            _dbContext.SaveChanges();
         }
 
         public IQueryable<TEntity> ReadAll()
         {
-            throw new NotImplementedException();
+            return _dbSet.AsQueryable();
         }
 
         public TEntity ReadByKey(object key)
         {
-            throw new NotImplementedException();
+            return _dbSet.Find(key);
         }
 
         public void Update(TEntity t, object key)
         {
-            throw new NotImplementedException();
+           TEntity existing = _dbSet.Find(key);
+            if (existing != null)
+            {
+                _dbContext.Entry(existing).CurrentValues.SetValues(t);
+                _dbContext.SaveChanges();
+                _dbContext.Entry(existing).Reload();
+            }
         }
+
+        public void Delete(object key)
+        {
+            TEntity existing = _dbSet.Find(key);
+            if (existing != null)
+            {
+                _dbSet.Remove(existing);
+                _dbContext.SaveChanges();
+            }
+        }
+
+
     }
 }
