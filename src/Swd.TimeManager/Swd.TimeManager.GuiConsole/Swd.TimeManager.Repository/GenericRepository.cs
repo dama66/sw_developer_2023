05997c636cc2a232499a4b8c8afcbf416b3f6a42
maierda <davidmaier66@gmail.com>
2024-01-16 19:22:30 +0100

    iGenericRepository

---
+++ src/Swd.TimeManager/Swd.TimeManager.GuiConsole/Swd.TimeManager.Repository/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swd.TimeManager.Repository
+{
+    public class GenericRepository<TEntity, TModel> : IGenericRepository<TEntity>
+        where TEntity : class, new() 
+        where TModel : DbContext, new()
+
+    {
+        public DbSet<TEntity> DbSet => throw new NotImplementedException();
+
+        public void Add(TEntity t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ReadByKey(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity t, object key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
